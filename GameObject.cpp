69849e30ceeaae6940d00b5fc6e29728bf6719b3
Akira1707 <thuhoai20021707@gmail.com>
2024-05-20 22:43:27 +0300

    Change

--- GameObject.cpp
+++
@@ -1,6 +0,0 @@
-#include "GameObject.h"
-
-GameObject::GameObject(Vector2f position_): position_(position_){}
-void GameObject::SetPosition(float x, float y) { position_ = Vector2f(x, y); }
-Vector2f GameObject::GetPosition() { return position_; }
-GameObject::~GameObject(){}
